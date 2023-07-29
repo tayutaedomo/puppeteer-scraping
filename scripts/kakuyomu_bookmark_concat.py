188d94f51b81a52b0d72872adac82809bbf18b79
tayutaedomo <tayutaedomo@gmail.com>
2023-07-29 16:39:47 +0900

    Add new output file of kakuyomu bookmark

--- scripts/kakuyomu_bookmark_concat.py
+++ scripts/kakuyomu_bookmark_concat.py
@@ -1,15 +1,28 @@
 import glob
 import json
+from collections import defaultdict
 
 file_paths = glob.glob("tmp/kakuyomu_bookmark_*.json")
 
-bookmarks = {}
+bookmarks_by_type = {}
 
 for file_path in file_paths:
   with open(file_path, 'r') as file:
       bookmark_type = file_path.split('/')[-1].replace('kakuyomu_bookmark_', '').replace('.json', '')
       print(bookmark_type, file_path)
-      bookmarks[bookmark_type] = json.load(file)
+      bookmarks_by_type[bookmark_type] = json.load(file)
 
-with open('tmp/kakuyomu_bookmarks.json', 'w') as f:
-    json.dump(bookmarks, f, indent=2, ensure_ascii=False)
+with open('tmp/kakuyomu_bookmarks_type.json', 'w') as f:
+    json.dump(bookmarks_by_type, f, indent=2, ensure_ascii=False)
+    print("Created kakuyomu_bookmarks_type.json")
+
+# ノベル毎にブックマークの種類をまとめてファイル出力する
+bookmarks_by_id = defaultdict(list)
+
+for bookmark_type, urls in bookmarks_by_type.items():
+  for url in urls:
+    bookmarks_by_id[url].append(bookmark_type)
+
+with open('tmp/kakuyomu_bookmarks_id.json', 'w') as f:
+    json.dump(bookmarks_by_id, f, indent=2, ensure_ascii=False)
+    print("Created kakuyomu_bookmarks_id.json")
